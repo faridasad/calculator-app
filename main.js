cb1b69e9bff1eb15a11f5961e929c6b4161a7cb4
faridasad <asadlifaridd@gmail.com>
2022-07-28 23:11:52 +0400

    Save preferred theme

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
-const output = document.querySelector(".output")
-const buttons = document.querySelectorAll("button")
+const output = document.querySelector(".output");
+const buttons = document.querySelectorAll("button");
 
-let isFinished = false
+let isFinished = false;
 buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        if(isFinished === true) {
-            output.textContent = '';
-            isFinished = false
+  button.addEventListener("click", (e) => {
+    if (isFinished === true) {
+      output.textContent = "";
+      isFinished = false;
+    }
+    switch (e.target.textContent) {
+      case "DEL":
+        output.textContent = output.textContent.slice(0, -1);
+        break;
+      case "RESET":
+        output.textContent = "";
+        break;
+      case "=":
+        try {
+          output.textContent = output.textContent.replace(/x/g, "*");
+          output.textContent = eval(output.textContent);
+        } catch (error) {
+          output.textContent = "TRY AGAIN";
+          isFinished = true;
         }
-        switch(e.target.textContent){
-            case 'DEL':
-                output.textContent = output.textContent.slice(0, -1)
-                break
-            case 'RESET':
-                output.textContent = ''
-                break
-            case '=':
-                try {
-                    output.textContent = output.textContent.replace(/x/g, '*')
-                    output.textContent = eval(output.textContent)
-                } catch(error) {
-                    output.textContent = "TRY AGAIN"
-                    isFinished = true
-                }
-                break
-            default:
-                output.textContent += e.target.textContent
-        }
-    })
-})
-
+        break;
+      default:
+        output.textContent += e.target.textContent;
+    }
+  });
+});
 
 /* Theme stuff */
 
-const themes = document.querySelectorAll(".theme")
-const body = document.querySelector("body")
+const themes = document.querySelectorAll(".theme");
+const body = document.querySelector("body");
+body.setAttribute("data-theme", localStorage.getItem("data-theme"));
 
 themes.forEach((theme) => {
-    theme.addEventListener("click", () => {
-        body.setAttribute("data-theme", theme.dataset.id)
-    })
-})
+  theme.addEventListener("click", () => {
+    body.setAttribute("data-theme", theme.dataset.id);
+    localStorage.setItem("data-theme", theme.dataset.id);
+  });
+});
